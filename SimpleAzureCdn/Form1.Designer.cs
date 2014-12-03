e9dc3feb893d70e009690e9dc5ba2b29649b64f2
Zeeshan Sheikh <zee@blanklabel.com>
2014-12-03 08:48:47 -0500

    Init

---
+++ SimpleAzureCdn/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SimpleAzureCdn
+{
+    partial class simpleAzureCdnForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectUploadFile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // selectUploadFile
+            // 
+            this.selectUploadFile.Location = new System.Drawing.Point(93, 46);
+            this.selectUploadFile.Name = "selectUploadFile";
+            this.selectUploadFile.Size = new System.Drawing.Size(156, 32);
+            this.selectUploadFile.TabIndex = 0;
+            this.selectUploadFile.Text = "Select and Upload file";
+            this.selectUploadFile.UseVisualStyleBackColor = true;
+            this.selectUploadFile.Click += new System.EventHandler(this.do_button_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // simpleAzureCdnForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 172);
+            this.Controls.Add(this.selectUploadFile);
+            this.Name = "simpleAzureCdnForm";
+            this.Text = "Simple File Select and Upload";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectUploadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ SimpleAzureCdn/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace SimpleAzureCdn
+{
+    public partial class simpleAzureCdnForm : Form
+    {
+        public simpleAzureCdnForm()
+        {
+            InitializeComponent();
+        }
+
+        static CloudStorageAccount storageAccount;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //gets connectionstring from app.config
+            storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+        }
+
+        private void do_button_Click(object sender, EventArgs e)
+        {
+            //simple dialog box to select a file
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+
+                try
+                {
+
+                    //create client to work with blobs
+                    CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+
+                    //already created container via azure management portal, set container reference
+                    CloudBlobContainer container = blobClient.GetContainerReference("SomeContainer");
+
+                    
+                    using (var fileStream = System.IO.File.OpenRead(openFileDialog1.FileName))
+                    {
+                        //set distination (will create or overwrite existing blob)
+                        CloudBlockBlob blockBlob = container.GetBlockBlobReference(openFileDialog1.SafeFileName);
+
+                        //upload from stream
+                        blockBlob.UploadFromStream(fileStream);
+                    } 
+
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            
+        }
+    }
+}
